796df7a9d2302d0d703a5f81c6759ae73dc2a22e
Atharva1583 <atharvajo31@gmail.com>
2022-06-11 00:50:10 +0530

    Files uploaded

---
+++ src/data.js
@@ -0,0 +1,28 @@
+export default [
+    {
+      id: 1,
+      name: 'Atharva',
+      age: 18,
+     
+    },
+    {
+      id: 2,
+      name: 'Baba',
+      age: 50,
+     
+    },
+    {
+      id: 3,
+      name: 'Aai',
+      age: 39,
+      
+    },
+    {
+      id: 4,
+      name: 'Vaishnavi',
+      age: 16,
+      
+    },
+    
+  ];
+  
